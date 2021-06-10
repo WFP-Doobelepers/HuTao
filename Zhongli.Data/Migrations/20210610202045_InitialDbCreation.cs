24cc2121c0edb39c4378a28e40ea9d665706ccc4
sabihoshi <sabihoshi.dev@gmail.com>
2021-06-11 04:25:54 +0800

    feat: Database migration

---
+++ Zhongli.Data/Migrations/20210610202045_InitialDbCreation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zhongli.Data.Migrations
+{
+    public partial class InitialDbCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
