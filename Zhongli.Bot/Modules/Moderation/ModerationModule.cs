0844c47734fa4206e639c1f7efdfee7060f8e0d0
sabihoshi <sabihoshi.dev@gmail.com>
2021-06-15 20:54:25 +0800

    refactor: Remove redundant code in Moderation module

--- Zhongli.Bot/Modules/Moderation/ModerationModule.cs
+++ Zhongli.Bot/Modules/Moderation/ModerationModule.cs
@@ -27,17 +27,14 @@ namespace Zhongli.Bot.Modules.Moderation
         [RequireAuthorization(AuthorizationScope.Warning)]
         public async Task WarnAsync(IGuildUser user, uint warnCount = 1, [Remainder] string? reason = null)
         {
-            var userEntity = await _db.Users.FindAsync(user.Id) ?? _db.Add(new GuildUserEntity(user)).Entity;
-            await _db.SaveChangesAsync();
+            var (action, userEntity) = await CreateReprimandAction(user, Reprimand.Warning, ModerationActionType.Added, reason);
+            var warnEntity = action.ToWarning(warnCount);
 
             var totalWarnings = userEntity.WarningHistory.Sum(w => w.Amount);
             userEntity.WarningCount = (int) (totalWarnings + warnCount);
 
-            var actionEntity = await CreateReprimandAction(user, Reprimand.Warning, ModerationActionType.Added, reason);
-            var warnEntity = actionEntity.ToWarning(warnCount);
-
             userEntity.WarningHistory.Add(warnEntity);
-            userEntity.ReprimandHistory.Add(actionEntity);
+            userEntity.ReprimandHistory.Add(action);
 
             await _db.SaveChangesAsync();
 
@@ -51,12 +48,8 @@ namespace Zhongli.Bot.Modules.Moderation
         [RequireAuthorization(AuthorizationScope.Ban)]
         public async Task BanAsync(IGuildUser user, uint deleteDays = 1, [Remainder] string? reason = null)
         {
-            var userEntity = await _db.Users.FindAsync(user.Id) ?? _db.Add(new GuildUserEntity(user)).Entity;
-            await _db.SaveChangesAsync();
-
-            var action = await CreateReprimandAction(user, Reprimand.Ban, ModerationActionType.Added, reason);
+            var (action, userEntity) = await CreateReprimandAction(user, Reprimand.Ban, ModerationActionType.Added, reason);
             var banAction = action.ToBan(deleteDays);
-
             userEntity.ReprimandHistory.Add(action);
             userEntity.BanHistory.Add(banAction);
 
@@ -71,10 +64,7 @@ namespace Zhongli.Bot.Modules.Moderation
         [RequireAuthorization(AuthorizationScope.Kick)]
         public async Task KickAsync(IGuildUser user, [Remainder] string? reason = null)
         {
-            var userEntity = await _db.Users.FindAsync(user.Id) ?? _db.Add(new GuildUserEntity(user)).Entity;
-            await _db.SaveChangesAsync();
-
-            var action = await CreateReprimandAction(user, Reprimand.Kick, ModerationActionType.Added, reason);
+            var (action, userEntity) = await CreateReprimandAction(user, Reprimand.Kick, ModerationActionType.Added, reason);
             var kickAction = action.ToKick();
 
             userEntity.ReprimandHistory.Add(action);
@@ -95,10 +85,7 @@ namespace Zhongli.Bot.Modules.Moderation
             if (serverEntity.MuteRoleId is null)
                 return;
 
-            var userEntity = await _db.Users.FindAsync(user.Id) ?? _db.Add(new GuildUserEntity(user)).Entity;
-            await _db.SaveChangesAsync();
-
-            var action = await CreateReprimandAction(user, Reprimand.Mute, ModerationActionType.Added, reason);
+            var (action, userEntity) = await CreateReprimandAction(user, Reprimand.Mute, ModerationActionType.Added, reason);
             var muteAction = action.ToMute(length);
 
             userEntity.ReprimandHistory.Add(action);
@@ -109,14 +96,14 @@ namespace Zhongli.Bot.Modules.Moderation
             await ReplyAsync($"{user} has been muted.");
         }
 
-        private async Task<ReprimandAction> CreateReprimandAction(IGuildUser user, Reprimand reprimand,
+        private async Task<(ReprimandAction, GuildUserEntity)> CreateReprimandAction(IGuildUser user, Reprimand reprimand,
             ModerationActionType type, string? reason = null)
         {
             var userEntity = await _db.Users.FindAsync(user.Id) ?? _db.Add(new GuildUserEntity(user)).Entity;
             var modEntity = await _db.Users.FindAsync(Context.User.Id);
             var guildEntity = await _db.Guilds.FindAsync(Context.Guild.Id);
 
-            return new ReprimandAction
+            return (new ReprimandAction
             {
                 Reprimand = reprimand,
                 Type      = type,
@@ -127,7 +114,7 @@ namespace Zhongli.Bot.Modules.Moderation
 
                 Reason = reason,
                 Date   = DateTimeOffset.UtcNow
-            };
+            }, userEntity);
         }
     }
 }
