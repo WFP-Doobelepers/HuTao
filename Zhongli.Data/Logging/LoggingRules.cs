e2bd09448ce0c9741e1dde9ef35340309b59500e
Maanas Bukkuri <msbukkuri@gmail.com>
2021-06-30 22:59:08 -0700

    feat: Created the Data Structures for Logging

---
+++ Zhongli.Data/Logging/LoggingRules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Zhongli.Data.Models.Discord;
+
+namespace Zhongli.Data.Logging
+{
+    public class LoggingRules
+    {
+        public Guid Id { get; set; }
+
+        public ulong GuildId { get; set; }
+
+        public ulong ModerationChannelId { get; set; }
+
+        public virtual GuildEntity Guild { get; set; }
+    }
+}
