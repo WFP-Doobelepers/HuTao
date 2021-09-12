042e8d3a22668c82253e83c22102de2dc7b8e2e5
sabihoshi <sabihoshi.dev@gmail.com>
2021-09-13 06:32:42 +0800

    refactor: Remove redundant RegexOptions.Compiled flag

--- Zhongli.Services/Moderation/CensorExtensions.cs
+++ Zhongli.Services/Moderation/CensorExtensions.cs
@@ -9,7 +9,7 @@ namespace Zhongli.Services.Moderation
     public static class CensorExtensions
     {
         public static Regex Regex(this ICensor censor)
-            => new(censor.Pattern, censor.Options |= RegexOptions.Compiled, TimeSpan.FromSeconds(1));
+            => new(censor.Pattern, censor.Options, TimeSpan.FromSeconds(1));
 
         public static string? CensoredMessage(this Censored censored)
             => (censored.Trigger as Censor)?.Regex()
