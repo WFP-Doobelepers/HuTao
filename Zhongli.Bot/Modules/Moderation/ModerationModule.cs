67a26b8a96759186aa27be8afff4723c4f29d2b7
krustykarbs <84251908+krustykarbs@users.noreply.github.com>
2021-09-29 00:39:21 -0400

    fix: add channel arg to slowmode command

--- Zhongli.Bot/Modules/Moderation/ModerationModule.cs
+++ Zhongli.Bot/Modules/Moderation/ModerationModule.cs
@@ -109,9 +109,8 @@ namespace Zhongli.Bot.Modules.Moderation
         [Summary("Set a slowmode in the channel.")]
         [RequireBotPermission(ChannelPermission.ManageChannels)]
         [RequireUserPermission(ChannelPermission.ManageChannels)]
-        public async Task SlowmodeAsync(TimeSpan? length = null)
+        public async Task SlowmodeAsync(ITextChannel channel, TimeSpan? length = null)
         {
-            var channel = (ITextChannel) Context.Channel;
             var seconds = (int) (length ?? TimeSpan.Zero).TotalSeconds;
 
             await channel.ModifyAsync(c => c.SlowModeInterval = seconds);
