03dccac9e4548e75487802db97fc091e19a40bc0
krustykarbs <84251908+krustykarbs@users.noreply.github.com>
2021-09-30 00:15:29 -0400

    refactor: Use Format.Bold instead of markdown

--- Zhongli.Data/Models/Moderation/Infractions/Actions/BanAction.cs
+++ Zhongli.Data/Models/Moderation/Infractions/Actions/BanAction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Humanizer;
+using Discord;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -14,7 +15,7 @@ namespace Zhongli.Data.Models.Moderation.Infractions.Actions
         }
 
         public override string Action
-            => $"Ban **{Length?.Humanize() ?? "indefinitely"}** and delete **{DeleteDays} days** of messages";
+            => $"Ban {Format.Bold(Length?.Humanize() ?? "indefinitely")} and delete {Format.Bold(DeleteDays + " days")} of messages";
 
         public uint DeleteDays { get; set; }
 
