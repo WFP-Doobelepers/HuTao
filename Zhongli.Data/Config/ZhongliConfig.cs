00f39255c773b312fa02525f1b2822ac3113de74
sabihoshi <sabihoshi.dev@gmail.com>
2022-01-07 14:13:26 +0800

    refactor: Remove unused methods in ZhongliConfig

--- Zhongli.Data/Config/ZhongliConfig.cs
+++ Zhongli.Data/Config/ZhongliConfig.cs
@@ -4,19 +4,17 @@ namespace Zhongli.Data.Config;
 
 public class ZhongliConfig
 {
-    public static readonly IConfigurationRoot Secrets = new ConfigurationBuilder()
-        .AddUserSecrets<ZhongliConfig>().Build();
+    private static readonly IConfigurationRoot Secrets
+        = new ConfigurationBuilder().AddUserSecrets<ZhongliConfig>().Build();
 
     public BotConfig Debug { get; init; } = null!;
 
     public BotConfig Release { get; init; } = null!;
 
-    public static T GetValue<T>(string key) => Secrets.GetSection("Debug").GetValue<T>(key);
-
     public static BotConfig Configuration { get; } =
 #if DEBUG
         Secrets.GetSection(nameof(Debug)).Get<BotConfig>();
 #else
-            Secrets.GetSection(nameof(Release)).Get<BotConfig>();
+        Secrets.GetSection(nameof(Release)).Get<BotConfig>();
 #endif
 }
