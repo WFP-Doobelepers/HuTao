18743175111ea9d30fbff17ddf5b7773762b5fce
sukaruta <sukaruta12@gmail.com>
2022-02-11 15:24:56 +0800

    fix: ping command get discord latency through message created timestamps

--- Zhongli.Bot/Modules/GeneralModule.cs
+++ Zhongli.Bot/Modules/GeneralModule.cs
@@ -31,20 +31,23 @@ public class GeneralModule : ModuleBase<SocketCommandContext>
     [Summary("Shows the ping latency of the bot.")]
     public async Task PingAsync()
     {
-        var sw = new Stopwatch();
-        sw.Start();
+        // var sw = new Stopwatch();
+        // sw.Start();
 
-        var latency = Context.Client.Latency.Milliseconds().Humanize(5);
+        var gatewayLatency = Context.Client.Latency.Milliseconds().Humanize(5);
         var embed = new EmbedBuilder()
             .WithTitle("Pong!")
             .WithUserAsAuthor(Context.User, AuthorOptions.Requested)
-            .AddField("Gateway Latency", $"{latency}")
+            .AddField("Gateway Latency", $"{gatewayLatency}")
             .WithCurrentTimestamp();
 
         var message = await ReplyAsync(embed: embed.Build());
 
-        sw.Stop();
-        embed.AddField("Discord Latency", sw.Elapsed.Humanize(5));
+        // sw.Stop();
+        var discordLatency
+            = (message.CreatedAt.ToUnixTimeMilliseconds() - Context.Message.CreatedAt.ToUnixTimeMilliseconds())
+            .Milliseconds().Humanize(5);
+        embed.AddField("Discord Latency", discordLatency);
 
         await message.ModifyAsync(m => m.Embeds = new[] { embed.Build() });
     }
