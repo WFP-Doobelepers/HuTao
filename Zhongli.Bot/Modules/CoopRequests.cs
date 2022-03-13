1ed8cf46cd472d270cf1a4db47b10e682ffc7702
Xyncross1111 <96194096+Xyncross1111@users.noreply.github.com>
2022-03-14 02:17:07 +0530

    feat(coop interaction): Add button interactions for request message

--- Zhongli.Bot/Modules/CoopRequests.cs
+++ Zhongli.Bot/Modules/CoopRequests.cs
@@ -1,9 +1,7 @@
-﻿using System.Text.RegularExpressions;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using Discord;
+﻿using Discord;
 using Discord.Commands;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Zhongli.Bot.Modules;
 
@@ -12,57 +10,45 @@ namespace Zhongli.Bot.Modules;
 
 public class CoopRequestsModule : ModuleBase<SocketCommandContext>
 {
-    [Command("req")]
-    [Summary("Make a request co-op request to other players")]
+    [Command("coop")]
+    [Alias("request","req")]
+    [Summary("Make a co-op request to other players")]
 
     public async Task RequestAsync([Remainder] string request)
     {
-        var region = Region(request);
         var uid = Uid(request);
+        var region = Region(uid);
 
-        var embed = new EmbedBuilder()
-            .WithTitle("Co-op Request")
-            .WithThumbnailUrl(Context.Message.Author.GetAvatarUrl())
-            .WithDescription(request)
-            .AddField($"Region:", inline: true, value: region)
-            .AddField("UID:", value: uid, true)
-            .WithFooter($"Requested by {Context.Message.Author.Username}")
-            .WithCurrentTimestamp();
-
-        var button = new ComponentBuilder()
-            .WithButton("Help", "help");
-
-        await ReplyAsync("", false, components: button.Build(), embed: embed.Build());
-
-
-    }
-    public string Region(string message)
-    {
-        message = message.ToLower();
-        var region = "";
-
-        if (message.Contains("asia"))
+        if (uid is null || region == "Unspecified")
         {
-            return region = "Asia";
-        }
-        else if (message.Contains("europe"))
-        {
-            return region = "Europe";
-        }
-        else if (message.Contains("na") || message.Contains("north america") || message.Contains("america"))
-        {
-            return region = "North America";
+            await ReplyAsync("Please Enter a valid UID.");
         }
         else
         {
-            return region = "Unspecified";
+            var embed = new EmbedBuilder()
+                .WithTitle("Co-op Request")
+                .WithThumbnailUrl(Context.Message.Author.GetAvatarUrl())
+                .WithDescription(request)
+                .AddField($"Region:", region, true)
+                .AddField("UID:", uid, true)
+                .WithFooter($"Requested by {Context.Message.Author.Username}")
+                .WithCurrentTimestamp()
+                .Build();
+
+            var buttons = new ComponentBuilder()
+                .WithButton("Help", $"help:{Context.User.Id}")
+                .WithButton("Close", $"close:{Context.User.Id}", ButtonStyle.Danger)
+                .Build();
+
+            await ReplyAsync($"<@&{RolePing(uid)}>", false, components: buttons, embed: embed);
         }
     }
-    public static string Uid(string text)
-    {
-        string Uid = @"\(?\d{9}\)?";
 
-        Match uid = Regex.Match(text,Uid);
+    private static string Uid(string text)
+    {
+        const string uidPattern = @"\(?\d{9}\)?";
+
+        var uid = Regex.Match(text,uidPattern);
         if (uid.Success)
         {
             return uid.Value;
@@ -73,7 +59,49 @@ public class CoopRequestsModule : ModuleBase<SocketCommandContext>
         }
     }
 
+    private static string Region(string? uid)
+    {
+        var regionId = "Unspecified";
 
-
+        switch (uid[0])
+        {
+            case '6':
+                regionId = "North America";
+                break;
+            case '7':
+                regionId = "Europe";
+                break;
+            case '8':
+                regionId = "Asia";
+                break;
+            case '9':
+                regionId = "SAR";
+                break;
+        }
+        return regionId;
     }
+
+    private static string RolePing(string? uid)
+    {
+        var roleId = "Unspecified";
+
+        //Enter the Role ID of the roles to be pinged, namely NA, Europe, Asia, SAR respectively.
+        switch (uid[0])
+        {
+            case '6':
+                roleId = "952476641165193216";
+                break;
+            case '7':
+                roleId = "952477465828282368";
+                break;
+            case '8':
+                roleId = "952476516158152704";
+                break;
+            case '9':
+                roleId = "952477656228708422";
+                break;
+        }
+        return roleId;
+    }
+}
 
--- Zhongli.Bot/Modules/InteractiveCoopModule.cs
+++ Zhongli.Bot/Modules/InteractiveCoopModule.cs
@@ -1,19 +1,64 @@
-﻿using Discord.Interactions;
-using Discord;
-
+﻿using Discord;
+using System.Linq;
+using Discord.Interactions;
 using System.Threading.Tasks;
-using Discord.Commands;
 
 namespace Zhongli.Bot.Modules;
 
 public class InteractiveCoopModule : InteractionModuleBase<SocketInteractionContext>
 {
-    [ComponentInteraction("help")]
-    public async Task HelpButtonHandler()
+    [ComponentInteraction("help:*")]
+
+    public async Task HelpButton(string id )
     {
-        await RespondAsync(
-            $"{Context.Interaction.User.Mention} has taken up this request on this button\nPlease follow the Co-op rules for an enjoyable and safe playing experience.");
+        if (Context.Interaction is IComponentInteraction interaction)
+        {
+            if (ulong.Parse(id) != Context.Interaction.User.Id)
+            {
+                var message = interaction.Message;
+                var embeds = message.Embeds.Select(e => e.ToEmbedBuilder()
+                    .WithColor(Color.Green)
+                    .AddField("Taken by:", $"{interaction.User.Mention}", true)
+                    .Build());
 
+                await message.ModifyAsync(m => m.Embeds = embeds.ToArray());
 
+                await RespondAsync(
+                    $"{Context.Interaction.User.Mention}Please follow the Co-op rules for an enjoyable and safe playing experience.\nDo Not Re-Click on the Help Button again.",
+                    ephemeral: true);
+            }
+            else
+            {
+                {
+                    await RespondAsync($"Can't help yourself now, Can you? {Context.Interaction.User.Mention}", ephemeral: true);
+                }
+            }
+        }
+    }
+
+    [ComponentInteraction("close:*")]
+
+    public async Task CloseButton(string id)
+    {
+        if (Context.Interaction is IComponentInteraction interaction)
+        {
+            if (ulong.Parse(id) == Context.Interaction.User.Id)
+            {
+                var message = interaction.Message;
+                var embeds = message.Embeds.Select(e => e.ToEmbedBuilder()
+                    .WithColor(Color.Red)
+                    .WithTitle("Co-op request [CLOSED]")
+                    .AddField("Request Closed by", $"{interaction.User.Mention}", true)
+                    .Build());
+
+                await message.ModifyAsync(m => m.Embeds = embeds.ToArray());
+            }
+            else
+            {
+                await RespondAsync(
+                    "Request can only be closed by the requesting individual.\nPlease ping a Moderator if you feel the request is over and should be closed.",
+                    ephemeral: true);
+            }
+        }
     }
 }
